ed587d2788d0a4bfcd38caff7da11a4941ee1f72
Sanketh Shinde <sankethshinde99@gmail.com>
2024-11-28 10:36:46 +0530

    created login form

--- src/components/AssignManager.jsx
+++ src/components/AssignManager.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { useState } from "react";
 
 const AssignManager = () => {
-  return <div>Assign Manager Page</div>;
+  const [managerId, setManagerId] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(managerId);
+    console.log(typeof managerId);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h1>Assign Manager</h1>
+        <div>
+          <label>
+            Manager:
+            <input
+              type="text"
+              placeholder="Enter Manager Id"
+              value={managerId}
+              onChange={(e) => {
+                setManagerId(e.target.value);
+              }}
+              required
+            />
+          </label>
+        </div>
+        <input type="submit" value="Assign" />
+      </form>
+    </>
+  );
 };
 
 export default AssignManager;
